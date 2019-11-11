54ac5c9450dd6349f435277e155465727fe6f99e
Giuseppe Crinò <giuseppe.crino@banksealer.com>
2019-11-11 11:36:41 +0100

    Wrap Int around UserId

--- src/main/scala/rps/GameController.scala
+++ src/main/scala/rps/GameController.scala
@@ -9,22 +9,22 @@ import io.buildo.enumero.{CaseEnumIndex, CaseEnumSerialization}
 @path("rps")
 trait GameController {
   @command
-  def play(userMove: Move): Future[Either[Throwable, Int]]
+  def play(userMove: Move): Future[Either[Throwable, UserId]]
 
   @query
-  def result(userId: Int): Future[Either[Throwable, PlayResponse]]
+  def result(id: Int): Future[Either[Throwable, PlayResponse]]
 }
 
 class GameControllerImpl(gameService: GameService)(implicit ec: ExecutionContext)
     extends GameController {
-  override def play(userMove: Move): Future[Either[Throwable, Int]] =
+  override def play(userMove: Move): Future[Either[Throwable, UserId]] =
     Future {
       Right(gameService.playMove(userMove))
     }
 
-  override def result(userId: Int): Future[Either[Throwable, PlayResponse]] =
+  override def result(id: Int): Future[Either[Throwable, PlayResponse]] =
     Future {
-      gameService.getResult(userId).map { play =>
+      gameService.getResult(UserId(id)).map { play =>
         PlayResponse(play.userMove, play.computerMove, play.result)
       }.toRight(new IllegalStateException)
     }
--- src/main/scala/rps/GameRepository.scala
+++ src/main/scala/rps/GameRepository.scala
@@ -6,21 +6,21 @@ import model._
 import java.util.concurrent.atomic.AtomicInteger
 
 trait GameRepository {
-  def save(play: Play): Int
-  def read(userId: Int): Option[Play]
+  def save(play: Play): UserId
+  def read(userId: UserId): Option[Play]
 }
 
 class InMemoryGameRepository extends GameRepository {
-  private val map = TrieMap.empty[Int, Play]
-  private var nextUserId = new AtomicInteger
+  private val map = TrieMap.empty[UserId, Play]
+  private var nextInt = new AtomicInteger
 
-  override def save(play: Play): Int = {
-    val userId = nextUserId.getAndIncrement
+  override def save(play: Play): UserId = {
+    val userId = UserId(nextInt.getAndIncrement)
     map.put(userId, play)
     userId
   }
 
-  override def read(userId: Int): Option[Play] =
+  override def read(userId: UserId): Option[Play] =
     map.get(userId)
 
 }
