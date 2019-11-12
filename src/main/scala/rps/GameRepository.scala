dc5f477eb9f688da00623cf148037753cc811a56
Giuseppe Crinò <giuseppe.crino@banksealer.com>
2019-11-12 12:57:07 +0100

    Leverage case class to get/put data in SQL db

--- src/main/scala/rps/GameRepository.scala
+++ src/main/scala/rps/GameRepository.scala
@@ -19,8 +19,8 @@ class InMemoryGameRepository extends GameRepository {
   private val map = TrieMap.empty[UserId, Play]
   private val db = Database.forConfig("h2mem1") // TODO: we should db.close this!
 
-  val plays = TableQuery[Plays]
-  val setupAction = plays.schema.create
+  val storedPlays = TableQuery[StoredPlays]
+  val setupAction = storedPlays.schema.create
   val setupFuture = db.run(setupAction)
   Await.result(setupFuture, Duration.Inf)
 
@@ -28,17 +28,17 @@ class InMemoryGameRepository extends GameRepository {
     val userMove = play.userMove
     val computerMove = play.computerMove
     val result = play.result
-    val plays = TableQuery[Plays]
-    val action = (plays returning plays.map(_.id)) +=
-      (0, userMove.toString(), computerMove.toString(), result.toString()) // https://stackoverflow.com/a/55269918/2219670
+    val storedPlays = TableQuery[StoredPlays]
+    val action = (storedPlays returning storedPlays.map(_.id)) +=
+      StoredPlay(Play(userMove, computerMove, result)) // https://stackoverflow.com/a/55269918/2219670
     val future = db.run(action)
     val id = Await.result(future, Duration.Inf) // assume this always succeeds
     UserId(id)
   }
 
   override def read(userId: UserId): Option[Play] = {
-    val plays = TableQuery[Plays]
-    val action = plays.filter(_.id === userId.id).result
+    val storedPlays = TableQuery[StoredPlays]
+    val action = storedPlays.filter(_.id === userId.id).result
     val future = db.run(action)
 
     val moveFromString = implicitly[CaseEnumSerialization[Move]].caseFromString _
@@ -46,22 +46,34 @@ class InMemoryGameRepository extends GameRepository {
 
     Await.result(future, Duration.Inf).toList match { // why a Vector is returned instead of a Seq?
       case Nil => None
-      case (_, userMoveAsString, computerMoveAsString, resultAsString) :: _ => { // assume only 1 row is returned
-        (moveFromString(userMoveAsString), moveFromString(computerMoveAsString), resultFromString(resultAsString)) match {
-          case (None, _, _) | (_, None, _) | (_, _, None) => None
-          case (Some(userMove), Some(computerMove), Some(result)) => Some(Play(userMove, computerMove, result))
-        }
-      }
+      case StoredPlay(play, _) :: _ => Some(play) // assume only 1 row is returned
     }
   }
 
 }
 
-class Plays(tag: Tag) extends Table[(Int, String, String, String)](tag, "PLAYS") {
-  def id: Rep[Int] = column[Int]("PLA_ID", O.PrimaryKey, O.AutoInc)
-  def userMove: Rep[String] = column[String]("PLA_USERMOVE")
-  def computerMove: Rep[String] = column[String]("PLA_COMPUTERMOVE")
-  def result: Rep[String] = column[String]("PLA_RESULT")
+case class StoredPlay(play: Play, id: Option[Int] = None)
 
-  def * : ProvenShape[(Int, String, String, String)] = (id, userMove, computerMove, result)
+class StoredPlays(tag: Tag) extends Table[StoredPlay](tag, "STOREDPLAYS") {
+  def userMove = column[String]("STO_USERMOVE")
+  def computerMove = column[String]("STO_COMPUTERMOVE")
+  def result = column[String]("STO_RESULT")
+
+  def id = column[Int]("STO_ID", O.PrimaryKey, O.AutoInc)
+
+  def sMove(m: String) = implicitly[CaseEnumSerialization[Move]].caseFromString(m).get
+  def sResult(r: String) = implicitly[CaseEnumSerialization[Result]].caseFromString(r).get
+
+  def * = (id.?, (userMove, computerMove, result)).shaped <> (
+    {
+      case (id, (um, cm, r)) => {
+        StoredPlay(Play.tupled.apply(sMove(um), sMove(cm), sResult(r)), id)
+      }
+    },
+    {
+      storedPlay: StoredPlay => {
+        Some((None, (storedPlay.play.userMove.toString, storedPlay.play.computerMove.toString, storedPlay.play.result.toString)))
+      }
+    }
+  )
 }
